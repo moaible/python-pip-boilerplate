e68ec1e6d8c4a588573d63d6b792ed484c847fc3
Eimer <moai.motodera@gmail.com>
2020-12-03 08:46:00 +0000

    Update setup package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-setup(name="pig",
+setup(name="boilerplate",
 	version="0.1",
 	description="Test for pip install git+",
 	url="https://github.com/moaible/python-pip-boilerplate",
