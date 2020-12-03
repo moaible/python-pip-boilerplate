75205dfd74a0b4e599b1207399b80d08383b1c60
Eimer <moai.motodera@gmail.com>
2020-12-03 14:51:09 +0000

    Change module name

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(name="executable-boilerplate",
 	version="0.1",
 	description="Test for pip install git+",
 	url="https://github.com/moaible/python-pip-executable-boilerplate",
+	install_requires=['library-boilerplate @ git+https://git@github.com/moaible/python-pip-library-boilerplate.git'],
 	packages=find_packages(),
 	entry_points="""
 	[console_scripts]
-	boilerplate = boilerplate.boilerplate:main
+	boilerplate = executable.executable:main
 	""",
 	)
