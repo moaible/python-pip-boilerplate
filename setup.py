07889afc09395564f5bb56cd4ba48e537edce803
Eimer <moai.motodera@gmail.com>
2020-12-03 08:53:46 +0000

    update package namae

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 
 from setuptools import setup, find_packages
 
-setup(name="boilerplate",
+setup(name="executable-boilerplate",
 	version="0.1",
 	description="Test for pip install git+",
-	url="https://github.com/moaible/python-pip-boilerplate",
+	url="https://github.com/moaible/python-pip-executable-boilerplate",
 	packages=find_packages(),
 	entry_points="""
 	[console_scripts]
